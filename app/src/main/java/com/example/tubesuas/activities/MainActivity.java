07b2057949c012f78a9dfe502fd9f678f1e7ea23
ALWAN <tsanyalwan@gmail.com>
2019-11-17 08:55:23 +0700

    Splash Screen, Welcome Activity

--- app/src/main/java/com/example/tubesuas/activities/MainActivity.java
+++ app/src/main/java/com/example/tubesuas/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tubesuas;
+package com.example.tubesuas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tubesuas.R;
 
 public class MainActivity extends AppCompatActivity {
 
